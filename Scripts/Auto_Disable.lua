5301f758911e85b5b1ca4c435cb98cb7202484e9
blaxpirit123 <denis.savr@mail.ru>
2014-11-20 13:15:46 +0300

    Update Auto_Disable.lua

--- Scripts/Auto_Disable.lua
+++ Scripts/Auto_Disable.lua
@@ -45,20 +45,22 @@ function Tick( tick )
 	local enemies = entityList:GetEntities({type=LuaEntity.TYPE_HERO,team = 5-me.team,alive=true,visible=true,illusion=false})
 		for i,v in ipairs(enemies) do
 			target = v
-			local IV = v:IsInvul()
-			local MI = v:IsMagicImmune()
-			local ST = v:IsStunned()
+			local IV  = v:IsInvul()
+			local MI  = v:IsMagicImmune()
+			local ST  = v:IsStunned()
 			local HEX = v:IsHexed()
-			local SI = v:IsSilenced()
+			local SI  = v:IsSilenced()
+			local DA  = v:IsDisarmed()
 			local invis = me:IsInvisible()
 			local blink = v:FindItem("item_blink")
 
-			if not (IV or MI or ST or HEX or SI or invis) then
+			if not (IV or MI or ST or HEX or SI or DA or invis) then
 				if blink and blink.cd > 11 then
 					UseHex()
 					UseSheepStickTarget()
 					UseAbyssaltarget()
 					UseOrchidtarget()
+					UseSilence()
 					UseEulScepterTarget()
 					UseHalberdtarget()
 					UseEtherealtarget()
@@ -68,6 +70,7 @@ function Tick( tick )
 					UseSheepStickTarget()
 					UseAbyssaltarget()
 					UseOrchidtarget()
+					UseSilence()
 					UseEulScepterTarget()
 					break
 				elseif Initiation[v.name] then
@@ -76,8 +79,9 @@ function Tick( tick )
 					if iSpell.level > 0 and iSpell.cd > iSpell:GetCooldown(iLevel) - 1 then
 						UseHex()
 						UseSheepStickTarget()
-						UseOrchidtarget()
 						UseAbyssaltarget()
+						UseOrchidtarget()
+						UseSilence()
 						UseEulScepterTarget()
 						UseHalberdtarget()
 						UseEtherealtarget()
@@ -205,16 +209,36 @@ end
 	
 function UseHex()
 	if activated == 0 then
-		local hex_lion = me:FindSpell("lion_voodoo")
-		local hex_rast = me:FindSpell("shadow_shaman_voodoo")
+		local hex_lion  = me:FindSpell("lion_voodoo")
+		local hex_rasta = me:FindSpell("shadow_shaman_voodoo")
 		if hex_lion then
-			skill = hex_lion
+			hex = hex_lion
 		elseif hex_rast then
-			skill = hex_rast
+			hex = hex_rasta
 		end
-		if skill and skill.level > 0 and skill:CanBeCasted() and me:CanCast() then
+		if hex and hex.level > 0 and hex:CanBeCasted() and me:CanCast() then
 			if target and GetDistance2D(me,target) < 500 then
-				me:SafeCastAbility(skill,target)
+				me:SafeCastAbility(hex,target)
+				activated = 1
+				sleepTick = GetTick() + 500
+				return
+			end
+		end
+	end
+end
+
+function UseSilence()
+	if activated == 0 then
+		local silence_sky = me:FindSpell("skywrath_mage_ancient_seal")
+		local silence_ns  = me:FindSpell("night_stalker_crippling_fear")
+		if silence_sky then
+			silence = silence_sky
+		elseif silence_ns then
+			silence = silence_ns
+		end
+		if silence and silence.level > 0 and silence:CanBeCasted() and me:CanCast() then
+			if target and GetDistance2D(me,target) < silence.castRange  then
+				me:SafeCastAbility(silence,target)
 				activated = 1
 				sleepTick = GetTick() + 500
 				return

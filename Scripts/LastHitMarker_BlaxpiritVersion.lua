a3ec930b947ca388d8818a3576fcb816d87b287a
Denis <denis.savr@mail.ru>
2014-12-15 23:19:50 +0300

    Update LastHitMarker_BlaxpiritVersion.lua

--- Scripts/LastHitMarker_BlaxpiritVersion.lua
+++ Scripts/LastHitMarker_BlaxpiritVersion.lua
@@ -1,12 +1,11 @@
 --<<If the icon has become a color that mean that creep dies from your attack>>
 --===Modified D.L.'s script By Blaxpirit===--
-
 require("libs.Utils")
 
 local rect = {}
 local sleep = 0
 local play = false
-local ex = client.screenSize.x/1600
+local ex = client.screenSize.x/1600*0.8
 
 function Tick( tick )
 
@@ -14,9 +13,8 @@ function Tick( tick )
 	
 	sleep = tick + 50
 	
-	local mydamage = me.dmgMin + me.dmgBonus
 	local damage = Damage()
-	local dmgtobuildings = 0.5*(mydamage)
+	local dmgtobuildings = 0.5*(me.dmgMin + me.dmgBonus)
 	
 	--========================<< ENTITIES >>======================================
 	local entities1 = {}
@@ -35,30 +33,33 @@ function Tick( tick )
 	for k,v in pairs(rastawards) do entities1[#entities1 + 1] = v end
 	for k,v in pairs(forge) do entities1[#entities1 + 1] = v end
 	for k,v in pairs(golem) do entities1[#entities1 + 1] = v end
-	--============================(\/)*_*(\/)======================================
+	--============================================================================
 	local entities2 = {}
 	
 	local siege = entityList:GetEntities({classId=CDOTA_BaseNPC_Creep_Siege})
-	
 	local towers = entityList:GetEntities({classId=CDOTA_BaseNPC_Tower})
 	local barracks = entityList:GetEntities({classId=CDOTA_BaseNPC_Barracks})
 	local buildings = entityList:GetEntities({classId=CDOTA_BaseNPC_Building})
 	------------------------------------------------------------------------------
 	for k,v in pairs(siege) do if v.spawned then entities2[#entities2 + 1] = v end end
-	
 	for k,v in pairs(towers) do entities2[#entities2 + 1] = v end
 	for k,v in pairs(barracks) do entities2[#entities2 + 1] = v end
 	for k,v in pairs(buildings) do entities2[#entities2 + 1] = v end
 	--============================================================================
+	--[[local roshan = entityList:FindEntities({classId=CDOTA_Unit_Roshan})
+	if roshan.alive and roshan.visible then 
+		v = roshan
+		LastHitMarker(v,damage)
+	end]]
+	--============================================================================
 	
 	for i, v in ipairs(entities1) do
-		LastHitMarker(v,damage,mydamage)
+		LastHitMarker(v,damage)
 	end
 
 	for i, v in ipairs(entities2) do
-		mydamage = dmgtobuildings
 		damage = dmgtobuildings
-		LastHitMarker(v,damage,mydamage)
+		LastHitMarker(v,damage)
 	end		
 end
 
@@ -72,23 +73,23 @@ function LastHitMarker(v,damage)
 			rect[v.handle] = drawMgr:CreateRect(-4*ex,-32*ex,0,0,0xFF8AB160) rect[v.handle].entity = v rect[v.handle].entityPosition = Vector(0,0,offset) rect[v.handle].visible = false 					
 		end
 
-		if v.visible and v.alive and v.health > 0  then						
-			if v.team == me.team and v.health < (mydamage*(1-v.dmgResist)+1) then
+		if v.visible and v.alive and v.health > 0 and v.health < (damage*(1-v.dmgResist)+1) then						
+			if v.team == me.team then
 				rect[v.handle].w = 20*ex
 				rect[v.handle].h = 20*ex
 				rect[v.handle].textureId = drawMgr:GetTextureId("NyanUI/other/Active_Deny")
-			elseif v.health < (damage*(1-v.dmgResist)+1) then
+			else
 				rect[v.handle].w = 15*ex
 				rect[v.handle].h = 15*ex
 				rect[v.handle].textureId = drawMgr:GetTextureId("NyanUI/other/Active_Coin")
 			end
 			rect[v.handle].visible = true
-		elseif v.visible and v.alive then					
-			if v.team == me.team and v.health > (mydamage*(1-v.dmgResist)) and v.health < (2*mydamage*(1-v.dmgResist)+1) then
+		elseif v.visible and v.alive and v.health > (damage*(1-v.dmgResist)) and v.health < (1.5*damage*(1-v.dmgResist)) then					
+			if v.team == me.team then
 				rect[v.handle].w = 20*ex
 				rect[v.handle].h = 20*ex
 				rect[v.handle].textureId = drawMgr:GetTextureId("NyanUI/other/Passive_Deny")
-			elseif v.health > (damage*(1-v.dmgResist)) and v.health < (2*damage*(1-v.dmgResist)) then
+			else
 				rect[v.handle].w = 15*ex
 				rect[v.handle].h = 15*ex
 				rect[v.handle].textureId = drawMgr:GetTextureId("NyanUI/other/Passive_Coin")

24da4cb527f9d143167c0850b324400ad6517a65
DLdota <SkyIce39@gmail.com>
2015-01-26 01:56:05 +0200

    improved code

--- Scripts/Juggernaut_WardControl.lua
+++ Scripts/Juggernaut_WardControl.lua
@@ -12,86 +12,81 @@ config:Load()
 local ward_move = config.MoveWard
 local ward_follow = config.FollowMyHero
 
-local reg = false
+local play = false
 local active = false
 
 local effect = nil
 
 function Key(msg,code)
-	if not PlayingGame() or client.chat then return end	
-	
-	if msg == KEY_UP then
-		if code == ward_move then
-			local me = entityList:GetMyHero()
-			local mp = entityList:GetMyPlayer()
+
+	if client.chat or msg ~= KEY_UP then return end	
+
+	if code == ward_move then
+		local me = entityList:GetMyHero()	
+		local ward = entityList:GetEntities({classId=297,alive = true,team=me.team})[1]
+		if ward then		
 			local cursor = client.mousePosition
-			local ward = entityList:GetEntities(function (v) return v.name == "npc_dota_juggernaut_healing_ward" and v.team == me.team and v.alive end)[1]
-			local allied_hero = entityList:GetEntities(function (v) return v.hero and v.alive and v.visible and not v:IsIllusion() and v.team == me.team and GetDistance2D(v,cursor) <= 100 end)[1]
-			if ward then
-				active = false
-				mp:Select(ward)
-				if allied_hero then 
-					mp:Follow(allied_hero)
-				else
-					mp:Move(client.mousePosition)
-				end
-				mp:Select(me)
+			local allied_hero = entityList:GetEntities(function (v) return v.hero and v.alive and v.visible and v.team == me.team and not v:IsIllusion() and GetDistance2D(v,cursor) <= 100 end)[1]
+			if allied_hero then 
+				ward:Follow(allied_hero)
+			else
+				ward:Move(client.mousePosition)
 			end
+			active = false
 		end
-		if code == ward_follow then
-			active = not active
-		end
+	elseif code == ward_follow then
+		active = not active
 	end
+
 end
 
 function Tick(tick)
-	if not client.connected or client.loading or client.console then return end
-	
-	if PlayingGame() and SleepCheck() then
-		local me = entityList:GetMyHero()
-		if not me then return end
-		if me.classId ~= CDOTA_Unit_Hero_Juggernaut then return end
-		
-		local mp = entityList:GetMyPlayer()
 
-		local ward = entityList:GetEntities(function (v) return v.name == "npc_dota_juggernaut_healing_ward" and v.team == me.team and v.alive end)[1]
-		
-		if ward and not effect then
+	if not (client.console or SleepCheck()) then return end
+
+	local me = entityList:GetMyHero()
+
+	local ward = entityList:GetEntities({classId=297,alive = true,team=me.team})[1]
+	
+	if ward then
+		if not effect then
 			effect = Effect(ward,"range_display")
 			effect:SetVector(1,Vector(500,0,0))
-		elseif not ward and effect then
-			effect = nil
-		end		
-
-		if active and ward and GetDistance2D(me,ward) > 5 then
-			mp:Select(ward)
-			mp:Move(me.position)
-			mp:Select(me)
-			Sleep(50)
 		end
-	end
+		if active and GetDistance2D(me,ward) > 5 then
+			ward:Move(me.position)			
+		end
+	elseif effect then
+		effect = nil
+		collectgarbage("collect")
+	end		
+	
+	Sleep(100)
+	
 end
 
 function Load()
-	if client.connected and not (client.loading or client.console) then
-		reg = true
-		script:RegisterEvent(EVENT_KEY,Key)
-		script:RegisterEvent(EVENT_TICK,Tick)
-		script:UnregisterEvent(Load)
+	if PlayingGame() then
+		local me = entityList:GetMyHero()
+		if me.classId == CDOTA_Unit_Hero_Juggernaut then		
+			play = true
+			script:RegisterEvent(EVENT_KEY,Key)
+			script:RegisterEvent(EVENT_TICK,Tick)
+			script:UnregisterEvent(Load)
+		else
+			script:Disable()
+		end
 	end
 end
 
 function GameClose()
-	active = false
-	effect = nil
-	collectgarbage("collect")
-	if reg then
+	if play then
 		script:UnregisterEvent(Key)
 		script:UnregisterEvent(Tick)
 		script:RegisterEvent(EVENT_TICK,Load)
-		reg = false
+		play = false
 	end
 end
 
-script:RegisterEvent(EVENT_CLOSE,GameClose)
 script:RegisterEvent(EVENT_TICK,Load)
+script:RegisterEvent(EVENT_CLOSE,GameClose)
